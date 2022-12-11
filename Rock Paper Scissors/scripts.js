80aa87d7f1a00ea563f6ed780e89b088e9b1d518
chia7812033 <51458240+chia7812033@users.noreply.github.com>
2022-12-11 14:20:09 +0800

    Add ui and style

--- Rock Paper Scissors/scripts.js
+++ Rock Paper Scissors/scripts.js
@@ -1,54 +1,65 @@
 const condition = ['rock', 'paper', 'scissors']
 
+let playScore = 0;
+let pcScore = 0;
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.dataset.type;
+        playRound(playerSelection, getComputerChoice())
+    })
+})
+
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     return condition[choice];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase(playerSelection);
     const playerIndex = condition.indexOf(playerSelection);
     const computerIndex = condition.indexOf(computerSelection);
     if (playerIndex === 2 && computerIndex === 0) {
-        return [0, `You Lose! ${computerSelection} beats ${playerSelection}`];
+        changeResult(0, playerSelection, computerSelection);
     }
     else if (playerIndex === 0 && computerIndex === 2) {
-        return [1, `You Win! ${playerSelection} beats ${computerSelection}`];
+        changeResult(1, playerSelection, computerSelection);
     }
     else if (playerIndex > computerIndex) {
-        return [1, `You Win! ${playerSelection} beats ${computerSelection}`];
+        changeResult(1, playerSelection, computerSelection);
     }
     else if (playerIndex < computerIndex) {
-        return [0, `You Lose! ${computerSelection} beats ${playerSelection}`];
+        changeResult(0, playerSelection, computerSelection);
     }
     else {
-        return [2, "Tie"];
+        changeResult(2, playerSelection, computerSelection);
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt();
-        const computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection)
-        console.log(result[1]);
-        if (result[0] === 0) {
-            computerScore++;
-        }
-        else if (result[0] === 1) {
-            playerScore++;
-        }
+function changeResult(win, playerSelection, computerSelection) {
+    const result = document.querySelector(".result")
+    if (win === 1) {
+        result.innerHTML = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playScore++;
+        playerScoreDiv = document.querySelector(".player-score").innerHTML = "Your score: " + parseInt(playScore);
     }
-
-    if (playerScore > computerScore) {
-        console.log('\nYou win');
-    }
-    else if (playerScore < computerScore) {
-        console.log('\nComputer win');
+    else if (win === 0) {
+        result.innerHTML = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        pcScore++;
+        pcScoreDiv = document.querySelector(".pc-score").innerHTML = "Computer score: " + parseInt(pcScore);
     }
     else {
-        console.log('\nNo winner');
+        result.innerHTML = `Tie!, ${computerSelection} and ${playerSelection}`;
+    }
+    findWinner();
+}
+
+function findWinner() {
+    if (playScore >= 5) {
+        document.querySelector(".final-result").innerHTML = "You are final winner!!!"
+    }
+    else if (pcScore >= 5) {
+        document.querySelector(".final-result").innerHTML = "Computer is final winner!!!"
     }
 }
