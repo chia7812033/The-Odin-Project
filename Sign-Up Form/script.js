4bed61c25514c5230bf98a5bc38e5c809cefdebe
chia7812033 <dan891221@gmail.com>
2022-12-17 18:37:31 +0800

    Sign up form

---
+++ Sign-Up Form/script.js
@@ -0,0 +1,12 @@
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+confirmPassword.addEventListener('input', () => {
+    console.log(password.value)
+    console.log(confirmPassword.value)
+    if (password.value != confirmPassword.value) {
+        document.querySelector(".error").textContent = "* Password do not match";
+    }
+    else {
+        document.querySelector(".error").textContent = "";
+    }
+})
